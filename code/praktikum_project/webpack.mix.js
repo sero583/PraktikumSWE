1a2b8b91bc631d07cc751cc5c3a9086c4d4cb7e7
JustRandomGerman <dresdani2002@gmail.com>
2022-05-16 12:07:51 +0200

    fix npm compiler error and css

--- code/praktikum_project/webpack.mix.js
+++ code/praktikum_project/webpack.mix.js
@@ -13,5 +13,5 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .react()
-    .postCss('resources/css/app.scss', 'public/css')
+    .postCss('resources/css/app.css', 'public/css')
     .setResourceRoot("/");
