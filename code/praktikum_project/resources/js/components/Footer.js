0bcd67722c04e243d5a08020f1b4c203425edead
Serhat Güler <serhatguler@hotmail.de>
2022-06-13 07:49:34 +0200

    Use singular for footer

--- code/praktikum_project/resources/js/components/Footer.js
+++ code/praktikum_project/resources/js/components/Footer.js
@@ -3,7 +3,7 @@ import '../../css/components/Footer.css';
 export default function Footer() {
     return (
         <div className="footer">
-            <p>&copy; 2022 Engineers-Trio. All Rights Reserved.</p>
+            <p>&copy; 2022 Engineer-Trio. All Rights Reserved.</p>
         </div>
     );
 }
