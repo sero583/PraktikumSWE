4d5cfa2b885a97705076ddb6bc00b64f581969be
JustRandomGerman <dresdani2002@gmail.com>
2022-06-19 22:49:31 +0200

    fix java lessons

--- code/praktikum_project/database/seeders/LessonSeed.php
+++ code/praktikum_project/database/seeders/LessonSeed.php
@@ -73,7 +73,7 @@ class LessonSeed extends Seeder
             "description"=>"In this lesson we will be talking about comparison operators. By using the comparison operators we can compare two variables with each other. We will be comparing two values with each other and see whether or not they are equal and also if one of them is greater than the other. The programm will return true if the value is equal and fals if the value is not equal. These true/false outputs are represented by the boolean data type and are used to differentiate whether a condition is true or false. For this lesson you are going to add the comparison operators (== and >) at the right print() statement between the a and b in the brackets.",
             "predefined_code"=>"<usercode>",
             "predefined_code_visible"=>"public class Main {\n\tpublic static void main(String[] args) {\n\t\tint a = 10;\n\t\tint b = 7;\n\n\t\tSystem.out.println(\"a equals b : \" + (a  b));\n\t\tSystem.out.print(\"a is greater than b : \" + (a  b));\n\t}\n}",
-            "expected_output"=>"a equals b : false\n a is greater than b : true",
+            "expected_output"=>"a equals b : false\na is greater than b : true",
             "xp"=>20,
             "language"=>"java"
 
@@ -83,10 +83,10 @@ class LessonSeed extends Seeder
             "course_id" => 1,
             "position" => 6,
             "title" => "Comparison Operators 2",
-            "description"=>"In the last lesson you learned how to compare variables of the data type int with each other. However the same does not apply to comparing Strings with each other. So therefore this lesson will cover how to compare the value of Strings with each other. It is possible o compare two Strings with each other by using the comparison operator (==) but this way you dont actually compare the value of the two Strings but rather the reference. So in order to compare the value of Strings, you are going to use the .equals() method. Enter one String behind the .equals and the other inside the brackets of the .equals() method.",
+            "description"=>"In the last lesson you learned how to compare variables of the data type int with each other. However the same does not apply to comparing Strings with each other. So therefore this lesson will cover how to compare the value of Strings with each other. It is possible to compare two Strings with each other by using the comparison operator (==) but this way you dont actually compare the value of the two Strings but rather the reference. So in order to compare the value of Strings, you are going to use the .equals() method. Enter one String behind the .equals and the other inside the brackets of the .equals() method.",
             "predefined_code"=>"<usercode>",
             "predefined_code_visible"=>"public class Main {\n\tpublic static void main(String[] args) {\n\t\tString a = \"Hello\";\n\t\tString b = \"hello\";\n\n\t\tSystem.out.println(\"a equals b : \" + .equals());\n\t}\n}",
-            "expected_output"=>"a equals b : false",
+            "expected_output"=>"a equals b : false\n",
             "xp"=>20,
             "language"=>"java"
 
@@ -97,7 +97,7 @@ class LessonSeed extends Seeder
             "position" => 7,
             "title" => "Methods",
             "description"=>"Now you will be learning about methods. A method is a block of code which only runs when it is called. You can pass data, known as parameters, into a method. Why use methods? To reuse code: define the code once, and use it many times. When declaring a method you specify the access of it (public), the return type (int), the name (multiplyNumbers) and the parameter list ((int a, int b)). You will be writing the body of the method called \"multiplyNumbers\". The method is supposed to return the multiplication (operator = *) of the variables a and b. The main method will run in the background and use the method with the values 10 and 15.",
-            "predefined_code"=>"public class Main {\n\tpublic static void main(String[] args) {\nint d = multiplyNumbers(10, 15);\n\t\tSystem.out.print(\"10 times 15 is : \" + d);\n\t}\n\n<usercode>\n}",
+            "predefined_code"=>"public class Main {\n\tpublic static void main(String[] args) {\nint d = multiplyNumbers(10, 15);\n\t\tSystem.out.print(\\\"10 times 15 is : \\\" + d);\n\t}\n\n<usercode>\n}",
             "predefined_code_visible"=>"public static int multiplyNumbers(int a, int b){\n\t\treturn ;\n\t}",
             "expected_output"=>"10 times 15 is : 150",
             "xp"=>20,
@@ -123,7 +123,7 @@ class LessonSeed extends Seeder
             "position" => 9,
             "title" => "Classes",
             "description"=>"Java is a so called object-oriented programming language. This means that in Java everything is connected with classes and objects, including their attributes (variables) and methods. Classes can be seen as a \"blueprint\" that defines how to create an object. To put this in a more real life oriented perspective: A car is an object, that has attributes like color or horsepower, and methods like drive, reverse or brake. In this lesson you will be creating a class called car class Car like this : class Car{<YourCode>} and assign it with the following 2 attributes: String color =\"blue\"; and int horsePower=300; . The main method will run in the background and return the attributes of your car class if you assigned them correctly.",
-            "predefined_code"=>"public class Main {\n\tpublic static void main(String[] args) {\n\t\tCar car = new Car();\n\t\tSystem.out.println(\"Car color : \" + car.color);\n\t\tSystem.out.print(\"Car horsepower : \" + car.horsePower);\n\t}\n}\n\n<usercode>",
+            "predefined_code"=>"public class Main {\n\tpublic static void main(String[] args) {\n\t\tCar car = new Car();\n\t\tSystem.out.println(\\\"Car color : \\\" + car.color);\n\t\tSystem.out.print(\\\"Car horsepower : \\\" + car.horsePower);\n\t}\n}\n\n<usercode>",
             "predefined_code_visible"=>"Enter Code",
             "expected_output"=>"Car color : blue\nCar horsepower : 300",
             "xp"=>20,
@@ -136,8 +136,8 @@ class LessonSeed extends Seeder
             "position" => 10,
             "title" => "Objects",
             "description"=>"This lesson will focus on Objects in java. In order for one to create an object, it is necessary to have a class that defines the attributes and methods of that object. For this lesson a class Person has already been created, that has the following 3 attributes: String name; int age; String bornIn; . You are going to create an instance of a person object and print out their attributes. To create a new Object do the following = Object object = new Object(); . All you need to do is fill out the missing parts and add the age and the bornIn attribute of the Person to the print statement just like how it is done in the first print statement",
-            "predefined_code"=>"class Person{\n\tString name = \"Harry\";\n\tint age = 23;\n\tString bornIn = \"Germany\";\n}",
-            "predefined_code_visible"=>"public class Main {\n\tpublic static void main(String[] args) {Person person =        ;\n\n\t\tSystem.out.println(\"Name : \" + person.Name);\n\t\tSystem.out.println(\"Age : \" + );\n\t\tSystem.out.print(\"Born in : \" + );\n\t}\n}",
+            "predefined_code"=>"public class Main {\n\tstatic class Person{\n\tString name = \\\"Harry\\\";\n\tint age = 23;\n\tString bornIn = \\\"Germany\\\";\n}<usercode>\n}",
+            "predefined_code_visible"=>"\npublic static void main(String[] args) {\n\tPerson person =        ;\n\n\tSystem.out.println(\"Name : \" + person.Name);\n\tSystem.out.println(\"Age : \" + );\n\tSystem.out.print(\"Born in : \" + );\n}",
             "expected_output"=>"Name : Harry\nAge : 23\nBorn in : Germany",
             "xp"=>20,
             "language"=>"java"
