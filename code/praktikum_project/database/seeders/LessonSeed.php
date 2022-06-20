ad11e5c832bfc3d7fc011f9d8af873225c3e6add
JustRandomGerman <dresdani2002@gmail.com>
2022-06-20 10:39:20 +0200

    fix merge problem in lesson seed

--- code/praktikum_project/database/seeders/LessonSeed.php
+++ code/praktikum_project/database/seeders/LessonSeed.php
@@ -167,40 +167,7 @@ class LessonSeed extends Seeder
             "expected_output"=>"1\nHello\n0.2\n",
             "xp"=>15,
             "language"=>"javascript",
-            // "course_id" =>1, wtf?
-            "position" =>1,
-            "title" => "Hello World in Java",
-            "description" => "Write a \"Hello World\" program in Java. In the Editor you can see all the Code necessary for doing that. The class is called Main because the program is written to the file \"Main.java\". All you have to do now is add the String you want to print into the print() statement.",
-            "predefined_code" => "<usercode>",
-            "predefined_code_visible" => "public class Main {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.print();\n\t}\n}",
-            "expected_output" => "Hello World",
-            "xp" => 20,
-            "language" => "java"
         ]);
-        Lesson::create([
-            "course_id" =>1,
-            "position" =>2,
-            "title" => "More sophisticated example",
-            "description" => "This is a more complicated example, where the user is supposed to implement a function, but this could also be a class etc. In this example the user is supposed to implement the function greet() that returns \"Hello World\" and all the stuff with the Main class and main Method is hidden from the user.",
-            "predefined_code" => "public class Main {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println(greet());\n\t}\n\t<usercode>\n}",
-            "predefined_code_visible" => "public static String greet(){\n\t\n}",
-            "expected_output" => "Hello World\n",
-            "xp" => 20,
-            "language" => "java"
-        ]);
-        Lesson::create([
-            "course_id" =>3,
-            "position" =>1,
-            "title" => "Hello World in Javascript",
-            "description" => "Write a \"Hello World\" program in Javascript. In the Editor you can see all the Code necessary for doing that. In Javascript you print using \"console.log\".",
-            "predefined_code" => "<usercode>",
-            "predefined_code_visible" => "console.log()",
-            "expected_output" => "Hello World\n",
-            "xp" => 15,
-            "language" => "javascript"
-        ]);
-
-        // other stuff not from me
         Lesson::create([
             "course_id"=>3,
             "position"=>4,
